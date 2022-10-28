5b6651f1fbdc319ad07cc3f481dee879da80a966
David Trelles <davidstrelles@gmail.com>
2022-10-27 22:34:24 -0500

    finally finished last stretch goal, still have to input album Data

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -117,16 +117,33 @@ function nAddToCollection(title, artist, yearPublished, tracks){
 let arr = [	"Wake Me Up Before You Go-Go : 3:50", "Everything She Wants : 5:01", "Heartbeat : 4:42"]
 console.log("Make It Big", nAddToCollection("Make it Big", "Wham!", 1984, arr))
 
-//   - Update `search` to allow a `trackName` search criteria.
-function nShowCollection (arr) {
-    console.log(arr.length);
-    for (let i =0; i<arr.length; i++) {
-        console.log(`${arr[i].title} by ${arr[i].artist}, published in ${arr[i].yearPublished}\n1. ${arr[i].tracks[0]}\n2. ${arr[i].tracks[1]}\n3. ${arr[i].tracks[2]}.`)
+//testing
+console.log(newCollection[0].tracks[1])
 
-    }
+//   - Update `search` to allow a `trackName` search criteria
+function nSearch(obj) { 
+    let newArr = [];
+    if(obj === undefined || Object.entries(obj).length === 0) {
+        return newCollection}
+        else {
+    for (let i = 0; i < newCollection.length; i++){
+        if (newCollection[i].artist === obj.artist && newCollection[i].yearPublished === obj.year){
+            for (let j = 0; j<newCollection[i].tracks.length;j++){
+                if(newCollection[i].tracks[j]===obj.track){
+                    newArr.push(newCollection[i].title)
+                }
+            }  
+        }
+    } return newArr
+  }
 }
 
-nShowCollection(newCollection)
+//test
+console.log(nSearch({artist: "David", year: 1997, track: "Caprice 17 : 2:35",}))
+console.log(nSearch())
+console.log(nSearch({}))
+console.log("This should work", nSearch({artist: "Wham!", year: 1984, track: "Heartbeat : 4:42",}))
+
 //   - Update the `showCollection` function to display the list of tracks 
 //   for each album with its name and duration.
 // ```
@@ -138,6 +155,14 @@ nShowCollection(newCollection)
 //     1. NAME: DURATION
 //     2. NAME: DURATION
 // ```
+function nShowCollection (arr) {
+    console.log(arr.length);
+    for (let i =0; i<arr.length; i++) {
+        console.log(`${arr[i].title} by ${arr[i].artist}, published in ${arr[i].yearPublished}\n1. ${arr[i].tracks[0]}\n2. ${arr[i].tracks[1]}\n3. ${arr[i].tracks[2]}.`)
 
+    }
+}
+//test
+nShowCollection(newCollection)
 // > Make sure to test all your code!
 
